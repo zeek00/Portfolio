a5f16d47fb4267d8e080721a3ff93abc6a3ecadd
zeek00 <44764168+zeek00@users.noreply.github.com>
2022-06-18 19:54:46 +0100

    Background, Drawer,Home ...starting social links, more modifications to the above when necessary

---
+++ zeekportfolio/src/components/Background/Bg.view.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../Background/Bg.style.css';
+import Home from "../Home/Home.view";
+function Bg() {
+  return (
+    <body>
+      <div className="bg-img">
+        <Home />
+      </div>
+        
+    </body>
+    
+   
+  );
+}
+
+export default Bg;
