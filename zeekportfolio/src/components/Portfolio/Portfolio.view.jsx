7feb3b6d013b2eca278140e05c724285996c1c0b
zeek00 <44764168+zeek00@users.noreply.github.com>
2022-12-20 19:55:54 +0000

    portfolio file modal setup begin

--- zeekportfolio/src/components/Portfolio/Portfolio.view.jsx
+++ zeekportfolio/src/components/Portfolio/Portfolio.view.jsx
@@ -5,14 +5,21 @@ import Drawer from "../Drawer/Drawer.view";
 import NavigationView from "../Navigation/Navigation.view";
 import File from './File'
 
+
 function Portfolio() {
-    const navigate = useNavigate();
-    const content =[
-        {name: 'Graphic Designs', link:'https://drive.google.com/drive/folders/1pZIW-rTXZBhxgW7v4qph4B6RPgQdqfYP?usp=sharing'},
-        {name: 'CV', link:'https://drive.google.com/drive/folders/1pZIW-rTXZBhxgW7v4qph4B6RPgQdqfYP?usp=share_link'},
-        {name: 'Projects', link: 'https://github.com/zeek00'},
+    useNavigate();
+    const content = [
+        {id:1, name: 'Graphic Designs', link:'https://drive.google.com/drive/folders/1pZIW-rTXZBhxgW7v4qph4B6RPgQdqfYP?usp=sharing'},
+        {id:2, name: 'CV', link:'https://drive.google.com/drive/folders/1pZIW-rTXZBhxgW7v4qph4B6RPgQdqfYP?usp=share_link'},
+        {id:3, name: 'Projects', link: 'https://github.com/zeek00'},
     ]
-    const [click, setClicked] = useState(false);
+
+    const contentViewer = (id)=>{
+        console.log('this is' + id);
+        
+    }
+    
+    
     return(
         <div className="portfolio">
             <div className="drawercomp">
@@ -20,13 +27,22 @@ function Portfolio() {
             </div>
             <div className="wrapper">
                 <NavigationView/>
+                
             </div>
-
+            
             <div className="portfolio-area">
-                {content.map(files => (
-                    <File name={files.name} link={files.link} />
-
+                {content.map((files, no) => (
+                    <File 
+                    name={files.name} 
+                    key={no} 
+                    title ={files.name}
+                    customClickEvent={()=>contentViewer(no)} />
+                    
                 ))}
+               
+                
+                
+                
             </div>
         </div>
 
